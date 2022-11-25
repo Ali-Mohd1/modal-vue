c8ced5b9e9efcd2f5d6bd9679d36cf56ec3801b0
ali-mohd <ali.beigh123@gmail.com>
2022-11-25 20:17:27 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <teleport to=".modals" v-if="showModal">
+      <Modal @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">Log in</a>
+        </template>
+        <h1>Log In</h1>
+        <form>
+          <input type="email" placeholder="Email " />
+          <input type="password" placeholder="......." />
+        </form>
+      </Modal>
+    </teleport>
+    <teleport to=".modals" v-if="showModalTwo">
+      <Modal @close="toggleModalTwo">
+        <template v-slot:links>
+          <a href="#">Get Started</a>
+        </template>
+        <h1>Create Account</h1>
+        <form>
+          <input type="text" placeholder="Full Name" />
+          <input type="email" placeholder="Email " />
+          <input type="password" placeholder="......." />
+        </form>
+      </Modal>
+    </teleport>
+    <div>
+      <button @click="toggleModal">Log In</button>
+      <button @click="toggleModalTwo">Sign Up</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "Modal Created Using Vue.Js",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+  },
+};
+</script>
+
+<style>
+#app,
+.modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+}
+button {
+  background: rgba(38, 71, 241);
+  padding: 10px 15px;
+  border: none;
+  border-radius: 10px;
+  font-size: 16px;
+  color: #fff;
+  margin: 10px 5px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+      <p class="close" @click="closeModal">X</p>
+      <slot></slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+<style>
+.modal {
+  position: relative;
+  width: 100%;
+  max-width: 500px;
+  padding: 20px 20px 40px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+.backdrop {
+  position: fixed;
+  top: 0;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+  padding: 0 15px;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+  color: #333;
+}
+.modal .actions a {
+  color: rgba(38, 71, 241);
+  padding: 8px 15px;
+  border: 1px solid rgba(38, 71, 241);
+  border-radius: 10px;
+  text-decoration: none;
+  margin: 10px;
+}
+.modal h1 {
+  color: rgba(38, 71, 241);
+  border: none;
+  padding: none;
+  margin-top: 20px;
+}
+p.close {
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  width: 25px;
+  height: 25px;
+  background: #333;
+  color: #ffff;
+  display: grid;
+  align-items: center;
+  border-radius: 50px;
+  font-weight: 700;
+  font-size: 12px;
+  cursor: pointer;
+}
+p.close:hover {
+  background: #222;
+}
+form input {
+  display: block;
+  width: 100%;
+  max-width: 350px;
+  margin: 10px auto;
+  height: 40px;
+  padding: 0 10px;
+  border: 1px solid #eee;
+  border-radius: 10px;
+}
+form input:focus {
+  outline-color: #ccc;
+}
+</style>
